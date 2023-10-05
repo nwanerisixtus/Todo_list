612d49b2ceb2a1fb255177298ffec5a7345e8d1c
nwanerisixtus <147055495+nwanerisixtus@users.noreply.github.com>
2023-10-05 04:54:02 -0700

    my first files

---
+++ todo.js
@@ -0,0 +1,10 @@
+let ptage= document.getElementById("demo")
+let input= document.getElementById("ent")
+let arr = []
+
+function  Add() {
+	arr.push(input.value)
+ document.getElementById("demo").innerText += arr;
+ input.value = " " 
+ .append(input.value)
+}
